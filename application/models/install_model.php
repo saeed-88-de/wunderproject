469d3f5ada2c34810c6c0054a0eaac0c21b66898
saeed <engsaeed88@yahoo.com>
2018-12-09 22:12:36 +0100

    install_model added

---
+++ application/models/install_model.php
@@ -0,0 +1,42 @@
+<?php
+
+class Install_model extends CI_Model
+{
+    public function create_db()
+    {
+        $query = "CREATE DATABASE IF NOT EXISTS wunderproject
+                  COLLATE = utf8_general_ci";
+        $this->db->query($query);
+        $query = "USE wunderproject";
+        $this->db->query($query);
+    }
+
+    public function create_tables()
+    {
+        $query = "CREATE TABLE IF NOT EXISTS users
+                  (
+                  id            INT(11) UNSIGNED    NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
+                  email_address VARCHAR(200)        NOT NULL,    UNIQUE,
+                  first_name    VARCHAR(50)         NOT NULL,
+                  last_name     VARCHAR(50)         NOT NULL,
+                  telephone     VARCHAR(22)         NOT NULL,
+                  password      VARCHAR(32)         NOT NULL,
+
+                  street        VARCHAR(50)         ,
+                  house_number  VARCHAR(10)         ,
+                  zip_code      VARCHAR(10)         ,
+                  city          VARCHAR(50)         ,
+
+                  account_owner VARCHAR(100)        ,
+                  iban          VARCHAR(30)         ,
+
+
+                  active        ENUM('0', '1')      NOT NULL    DAFAULT '0',
+                  KEY (first_name, last_name)
+                  )
+                  ENGINE = InnoDB   DEFAULT CHARSET = utf8  COLLATE = utf8_general_ci";
+        $this->db->query($query);
+    }
+}
+
+?>
